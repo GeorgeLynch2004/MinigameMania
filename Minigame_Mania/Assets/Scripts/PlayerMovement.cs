5df59fc36082160c179c35bcde9b51c75f2ddf3f
George <LynchG8@Cardiff.ac.uk>
2023-12-08 15:40:06 +0000

    commit

--- Minigame_Mania/Assets/Scripts/PlayerMovement.cs
+++ Minigame_Mania/Assets/Scripts/PlayerMovement.cs
@@ -9,16 +9,12 @@ public class PlayerMovement : NetworkBehaviour {
     [SerializeField] private Rigidbody2D _Rigidbody;
     [SerializeField] private float _MovementSpeed;
 
-    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
     private void FixedUpdate() 
     {
-        Debug.Log(OwnerClientId + ": " + randomNumber.Value);
         if (!IsOwner) return;
 
         if (Input.GetKey(KeyCode.A)){_Rigidbody.velocity = Vector2.left * _MovementSpeed;}
         if (Input.GetKey(KeyCode.D)){_Rigidbody.velocity = Vector2.right * _MovementSpeed;}
-
-        if (Input.GetKeyDown(KeyCode.T)){randomNumber.Value = Random.Range(0,100);}
     }
 }
